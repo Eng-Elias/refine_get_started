fa8ec922502da5365b3973c09d2e9851261723fb
Eng. Elias <eng.elias.owis@gmail.com>
2024-05-20 23:04:32 +0400

    refactoring

--- src/pages/products/show.tsx
+++ src/pages/products/show.tsx
@@ -1,13 +1,63 @@
-import { useShow } from "@refinedev/core";
+import { useShow, useOne } from "@refinedev/core";
+import {
+  TextFieldComponent as TextField,
+  NumberField,
+  MarkdownField,
+} from "@refinedev/mui";
+
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 
 export const ShowProduct = () => {
   const {
     queryResult: { data, isLoading },
   } = useShow();
 
+  const { data: categoryData, isLoading: categoryIsLoading } = useOne({
+    resource: "categories",
+    id: data?.data?.category.id || "",
+    queryOptions: {
+      enabled: !!data?.data,
+    },
+  });
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  return <div>Product name: {data?.data.name}</div>;
+  return (
+    <Stack gap={1}>
+      <Typography variant="body1" fontWeight="bold">
+        Id
+      </Typography>
+      <TextField value={data?.data?.id} />
+
+      <Typography variant="body1" fontWeight="bold">
+        Name
+      </Typography>
+      <TextField value={data?.data?.name} />
+
+      <Typography variant="body1" fontWeight="bold">
+        Description
+      </Typography>
+      <MarkdownField value={data?.data?.description} />
+
+      <Typography variant="body1" fontWeight="bold">
+        Material
+      </Typography>
+      <TextField value={data?.data?.material} />
+
+      <Typography variant="body1" fontWeight="bold">
+        Category
+      </Typography>
+      <TextField
+        value={categoryIsLoading ? "Loading..." : categoryData?.data?.title}
+      />
+
+      <Typography variant="body1" fontWeight="bold">
+        Price
+      </Typography>
+      <NumberField value={data?.data?.price} />
+    </Stack>
+  );
 };
