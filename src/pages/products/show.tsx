2d62cb751fe206e7321abd2769ac8000512422db
Eng. Elias <eng.elias.owis@gmail.com>
2024-05-20 23:53:24 +0400

    CRUD components

--- src/pages/products/show.tsx
+++ src/pages/products/show.tsx
@@ -1,63 +1,94 @@
-import { useShow, useOne } from "@refinedev/core";
-import {
-  TextFieldComponent as TextField,
-  NumberField,
-  MarkdownField,
-} from "@refinedev/mui";
+import { useForm } from "@refinedev/react-hook-form";
+import { Edit, useAutocomplete } from "@refinedev/mui";
 
-import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
-export const ShowProduct = () => {
+import { Controller } from "react-hook-form";
+
+export const EditProduct = () => {
   const {
-    queryResult: { data, isLoading },
-  } = useShow();
+    register,
+    control,
+    saveButtonProps,
+    refineCore: { queryResult },
+    formState: { errors },
+  } = useForm();
 
-  const { data: categoryData, isLoading: categoryIsLoading } = useOne({
+  const { autocompleteProps } = useAutocomplete({
     resource: "categories",
-    id: data?.data?.category.id || "",
-    queryOptions: {
-      enabled: !!data?.data,
-    },
+    defaultValue: queryResult?.data?.data?.category?.id,
   });
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <Stack gap={1}>
-      <Typography variant="body1" fontWeight="bold">
-        Id
-      </Typography>
-      <TextField value={data?.data?.id} />
-
-      <Typography variant="body1" fontWeight="bold">
-        Name
-      </Typography>
-      <TextField value={data?.data?.name} />
-
-      <Typography variant="body1" fontWeight="bold">
-        Description
-      </Typography>
-      <MarkdownField value={data?.data?.description} />
-
-      <Typography variant="body1" fontWeight="bold">
-        Material
-      </Typography>
-      <TextField value={data?.data?.material} />
-
-      <Typography variant="body1" fontWeight="bold">
-        Category
-      </Typography>
-      <TextField
-        value={categoryIsLoading ? "Loading..." : categoryData?.data?.title}
-      />
-
-      <Typography variant="body1" fontWeight="bold">
-        Price
-      </Typography>
-      <NumberField value={data?.data?.price} />
-    </Stack>
+    <Edit saveButtonProps={saveButtonProps}>
+      <Box
+        component="form"
+        sx={{ display: "flex", flexDirection: "column", gap: "12px" }}
+        autoComplete="off"
+      >
+        <TextField
+          {...register("name")}
+          label="Name"
+          error={!!errors.name}
+          helperText={errors.name?.message}
+        />
+        <TextField
+          {...register("description")}
+          multiline
+          label="Description"
+          error={!!errors.description}
+          helperText={errors.description?.message}
+        />
+        <TextField
+          {...register("material")}
+          label="Material"
+          error={!!errors.material}
+          helperText={errors.material?.message}
+        />
+        <Controller
+          control={control}
+          name="category"
+          defaultValue={null}
+          render={({ field }) => (
+            <Autocomplete
+              id="category"
+              {...autocompleteProps}
+              {...field}
+              onChange={(_, value) => field.onChange(value)}
+              getOptionLabel={(item) => {
+                return (
+                  autocompleteProps?.options?.find(
+                    (option) => option?.id == item?.id,
+                  )?.title ?? ""
+                );
+              }}
+              isOptionEqualToValue={(option, value) => {
+                return (
+                  value === undefined || option?.id == (value?.id ?? value)
+                );
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Category"
+                  variant="outlined"
+                  margin="normal"
+                  error={!!errors.category}
+                  helperText={errors.category?.message}
+                />
+              )}
+            />
+          )}
+        />
+        <TextField
+          {...register("price")}
+          label="Price"
+          error={!!errors.price}
+          helperText={errors.price?.message}
+        />
+      </Box>
+    </Edit>
   );
 };
